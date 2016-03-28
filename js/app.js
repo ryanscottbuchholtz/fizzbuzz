9c37b829322c2ea1e783fe48ac65db891453ac6f
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-28 15:06:48 -0700

    string fix for input

--- js/app.js
+++ js/app.js
@@ -2,23 +2,23 @@ var startNum = prompt("Where would you like to start?");
 var endNum = prompt("Where would you like to end?");
 var numList = document.getElementById('number-output');
 
-for (var i = startNum; i <= endNum; i++) {
+for (var i = startNum | 0; i <= endNum | 0; i++) {
   
   var listItem = document.createElement('li');
-  listItem.innerText = i;
   
   if (i%3 === 0 && i%5 === 0) {
     listItem.innerText = 'FizzBuzz';
-    numList.appendChild(listItem);
     }
   else if (i%5 === 0) {
     listItem.innerText = 'Buzz';
-    numList.appendChild(listItem);
   }
   else if (i%3 === 0) {
     listItem.innerText = 'Fizz';
-    numList.appendChild(listItem);
   }
-  else
-    numList.appendChild(listItem);
+  else {
+    listItem.innerText = i;
   }
+
+  numList.appendChild(listItem);
+
+}
