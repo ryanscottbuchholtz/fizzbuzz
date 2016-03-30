758e4d3aba646ec19f6ad536ab161cfbf9ea3105
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-30 08:52:23 -0700

    refactor prompt to validate end num greater than start num

--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
-var startNum = prompt("Where would you like to start?");
-var endNum = prompt("Where would you like to end?");
 var numList = document.getElementById('number-output');
 
-for (var i = startNum | 0; i <= endNum | 0; i++) {
-  
-  var listItem = document.createElement('li');
-  
-  if (i%3 === 0 && i%5 === 0) {
-    listItem.innerText = 'FizzBuzz';
-  }
-  else if (i%5 === 0) {
-    listItem.innerText = 'Buzz';
-  }
-  else if (i%3 === 0) {
-    listItem.innerText = 'Fizz';
-  }
-  else {
-    listItem.innerText = i;
-  }
-
-  numList.appendChild(listItem);
-
+function promptBegin () {
+  startNum = prompt("Where would you like to start?");
+  startNum = startNum | 0;
 }
+
+function promptEnd () {
+  endNum = prompt("Where would you like to end?");
+  endNum = endNum | 0;
+}
+
+function validateEndNum () {
+  promptBegin();
+  promptEnd();
+  while (endNum <= startNum) {
+    alert("This number must be greater than " + startNum);
+    promptEnd();
+  }
+}
+
+function fizzBuzz () {
+
+  validateEndNum();
+
+  for (var i = startNum; i <= endNum; i++) {
+
+    var listItem = document.createElement('li');
+    
+    if (i%3 === 0 && i%5 === 0) {
+      listItem.innerText = 'FizzBuzz';
+    }
+    else if (i%5 === 0) {
+      listItem.innerText = 'Buzz';
+    }
+    else if (i%3 === 0) {
+      listItem.innerText = 'Fizz';
+    }
+    else {
+      listItem.innerText = i;
+    }
+
+    numList.appendChild(listItem);
+
+  }
+}
+
+fizzBuzz();
+
+
+
